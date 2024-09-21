65c3378393a28aec8a793577363dc35b73ddfd05
Ian Gaona <pizzalovingguy@gmail.com>
2024-09-20 23:07:26 -0400

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,22 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-    <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <p>Hi there! I'm not a proud creator of this site, which I built using 
+      Gatsby's pisspoor YouTube tutorials. I wish they'd known how to film
+      tutorials and just do seperate livestreams. Using a livestream for both
+      is stupid, you should've observed and taken in what people needed in chat
+      and film a tutorial using what you clearly see beginners struggle with</p>
   </Layout>
   )
 }
 
-export const Head = () => <title>About Junji Ito</title>
+export const Head = () => <Seo title="About Junji Ito" />
 
 // Step 3: Export your component
 export default AboutPage
